f71f52ae8f6931d990d9adcbe20024f7a07e8697
Marcin <marcin@silversoft.pl>
2013-08-13 11:08:42 +0200

    export handle_dispatch/3

--- src/support/z_sites_dispatcher.erl
+++ src/support/z_sites_dispatcher.erl
@@ -28,7 +28,7 @@
 
 %% gen_server exports
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
--export([start_link/0, start_link/1]).
+-export([start_link/0, start_link/1, handle_dispatch/3]).
 
 %% interface functions
 -export([
